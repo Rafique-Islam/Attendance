d169a3040e465652209756841a8ab134e8467aa9
Rafique Islam <rafiquernrislam@gmail.com>
2019-12-17 22:05:57 -0500

    Initial commit

---
+++ db/conn.php
@@ -0,0 +1,37 @@
+<?php 
+  
+    // Development Connection local
+    //$host = '127.0.0.1';
+    //$db = 'attendance';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
+    
+    //Remote connect
+    $host = 'remotemysql.com';
+    $db = 'FHyKT6zDPa';
+    $user = 'FHyKT6zDPa';
+    $pass = 'lRjSkBtiOF';
+    $charset = 'utf8mb4';
+    
+    
+    
+    
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        'echo Hello you are connected';
+       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+   throw new PDOException($e->getMessage());
+    }
+ 
+    require_once 'crud.php';
+    require_once 'user.php';
+    
+    $crud = new crud($pdo);
+   
+
+    
+?>
